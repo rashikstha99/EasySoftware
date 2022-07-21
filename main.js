5fa287f9c40a5b4fc585275de5c2fdf9f07f73fc
Rashik Shrestha <rashikshrestha99@gmail.com>
2022-07-21 13:33:47 +0545

    first

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
     loop:true,
     margin:10,
     nav:false,
-    autoplay:false,
+    autoplay:true,
     autoplayTimeout:3000,
     responsive:{
         0:{
@@ -20,3 +20,25 @@
         }
     }
 })
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
